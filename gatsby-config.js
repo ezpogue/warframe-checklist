90714b4322abbe787f7c231ec9faf5d84db64591
ezpogue <ezpogue@gmail.com>
2025-04-18 13:36:09 -0700

    Prepare for deployment

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteMetadata: {
+      title: `Warframe Checklist`,
+      description: `Basic checklist for warframe items`,
+      author: `Ezekiel Pogue`,
+      siteUrl: `https://ezpogue.github.io/warframe-checklist`,
+    },
+  }
